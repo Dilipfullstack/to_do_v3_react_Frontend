99c2554ee24ef28443e27948ad42c5c764d720f7
Dilip Sahani <dilipsahani1894@gmail.com>
2023-01-25 20:05:35 -0800

    Deployment Link Added

--- src/variables.js
+++ src/variables.js
@@ -1,8 +1,19 @@
+// For local machine
+
+// const backendLink = {
+//     allTask: "http://localhost:3001/allTask",
+//     addTask: "http://localhost:3001/addTask",
+//     updateTask: "http://localhost:3001/updateTask",
+//     deleteTask: "http://localhost:3001/deleteTask"
+// }
+
+// For Deployment Online
+
 const backendLink = {
-    allTask: "http://localhost:3001/allTask",
-    addTask: "http://localhost:3001/addTask",
-    updateTask: "http://localhost:3001/updateTask",
-    deleteTask: "http://localhost:3001/deleteTask"
+    allTask: "https://to-do-apis.onrender.com/allTask",
+    addTask: "https://to-do-apis.onrender.com/addTask",
+    updateTask: "https://to-do-apis.onrender.com/updateTask",
+    deleteTask: "https://to-do-apis.onrender.com/deleteTask"
 }
 
 export default backendLink;
